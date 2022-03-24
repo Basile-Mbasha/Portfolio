c5822cff49ce54ec36e3e14d25b262291f2b0e9e
Basile Mbasha <95984302+Basile-Mbasha@users.noreply.github.com>
2022-03-23 22:22:00 -0400

    Add files via upload
    # Heart design using turtle graphics ~

---
+++ graphic_design.py
@@ -0,0 +1,23 @@
+#-user: Basile M.
+#-Date: March 23rd,2022 3:50PM
+# __Heart Design__ in Python using Turtle graphics
+
+
+from turtle import *
+
+bgcolor("white")
+color("red")
+begin_fill()
+
+pensize(3)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+
+#___happy_coding,....
+
+
